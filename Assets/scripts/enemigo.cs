6222ce19f1f569bcc8163e4c2a5d65f8b54fc36b
Kazui97 <camyescobar12@gmail.com>
2019-10-30 13:30:36 -0500

    aa

--- Assets/scripts/enemigo.cs
+++ Assets/scripts/enemigo.cs
@@ -9,22 +9,73 @@ public class enemigo : MonoBehaviour
     int m;
     void Start()
     {
-        m = Random.Range(0,1);
+        m = Random.Range(0,2);
         
     }
 
 
     public void Moverenemigo()
     {
-        transform.position -= transform.right = pos;
+       if ( m == 0)
+       {
+            if (gameObject.transform.position.y >= 75)
+            {
+                gameObject.transform.position -= new Vector3(0, 10 * Time.deltaTime, 0);
+            }
+            if (gameObject.transform.position.x <= 75)
+            {
+                mov *= 1;
+                if (gameObject.transform.position.x <= -75)
+                {
+                    mov *= -1;
+                }
+            }
+            else
+            {
+                mov *= -1;
+            }
+            gameObject.transform.position -= new Vector3(mov * Time.deltaTime, 0, 0);
+
+       }
+       else if (m == 1)
+        {
+            if (gameObject.transform.position.y <= 75)
+            {
+                gameObject.transform.position += new Vector3(0, 10 * Time.deltaTime, 0);
+            }
+            if (gameObject.transform.position.x >= 75)
+            {
+                mov *= -1;
+                if (gameObject.transform.position.x >= -75)
+                {
+                    mov *= 1;
+                }
+            }
+            else
+            {
+                mov *= 1;
+            }
+            gameObject.transform.position += new Vector3(mov * Time.deltaTime, 0, 0);
+
+        }
+
+
     }
 
     void Update()
     {
+        if (m == 0)
+        {
+            mov *= 1;
+        }
+        else
+        {
+       
+        }
         
         if (gameObject.transform.position.y >= 75)
         {
-            gameObject.transform.position -= new Vector3(0, 2 * Time.deltaTime, 0);
+            gameObject.transform.position -= new Vector3(0, 10 * Time.deltaTime, 0);
         }
         if (gameObject.transform.position.x <= 75)
         {
